07fc4067eb46dde4f64d81bc45a5b43000d9c971
Zarankur <pironchristophe92@gmail.com>
2019-09-15 21:13:30 +0200

    registerevents and another pinch try

---
+++ components/registerevents.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        var marker = this.el;
+
+        marker.addEventListener('markerFound', function() {
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
+            // TODO: Add your own code here to react to the marker being found.
+        });
+
+        marker.addEventListener('markerLost', function() {
+            var markerId = marker.id;
+            console.log('markerLost', markerId);
+            // TODO: Add your own code here to react to the marker being lost.
+        });
+    }
+});
