ec4070683447bb8911cc07af61b7f4b731cce3a7
Zarankur <pironchristophe92@gmail.com>
2019-12-29 00:51:52 +0100

    local ip

--- js/ui.js
+++ js/ui.js
@@ -23,7 +23,7 @@ const aframe_gui = document.querySelector('.aframe_gui');
 //   device.digitalWrite(4, 1);
 
 // });
-const url = 'https://esp8266.local/' //'//192.168.1.14/'; //'//192.168.0.174:1880/hello';     // '//192.168.1.14/';
+const url = 'https://192.168.1.14/'//'https://esp8266.local/' //'//192.168.1.14/'; //'//192.168.0.174:1880/hello';     // '//192.168.1.14/';
 
 function press_button(name){
   const xhttp = new XMLHttpRequest();
